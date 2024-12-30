0b8e48c5ae378ee41e515946b485d5fe0e859f23
Jonathan Powell <jprophit@gmail.com>
2024-12-30 10:12:37 +0000

    update

--- Learn Python By building games - Full Course/pong.py/Pong/connect four.py
+++ Learn Python By building games - Full Course/pong.py/Pong/connect four.py
@@ -9,6 +9,18 @@ def create_board(
     board = np.zeros((6,7)))
     return board
 
+# drops the player 1 or player 2 piece
+def drop_piece()
+    pass
+
+# defines if its the valid location
+def is_valid_location():
+    pass
+
+# gets the next open row
+def get_next_open_row():
+    pass
+
 # creates board
 board = create_board()
 
@@ -22,6 +34,17 @@ turn = 0
 while not game_over:
     # ask for player 1 input
     if turn == 0:
-        selection = input("Player 1 make your Selection (0-6):")
+        selection = int(input("Player 1 make your Selection (0-6):"))
 
+        '''
+        print(selection)
+        print(type(selectiojn))
+        '''
     # askfor player 2 input
+    else:
+        selection = int(input("Player 2 make your Selection (0-6):"))
+
+    # increase turn by 1
+    turn += 1
+    # turns its to odd even, meaning take what ever our turn is and divide it by 2 (alternating between player 1 and 2)
+    turn = turn % 2
