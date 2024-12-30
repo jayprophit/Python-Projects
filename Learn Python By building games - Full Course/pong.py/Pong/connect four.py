feb2b3675ea8dca1eaee921175e0317db6fc9162
Jonathan Powell <jprophit@gmail.com>
2024-12-30 09:54:22 +0000

    update

--- Learn Python By building games - Full Course/pong.py/Pong/connect four.py
+++ Learn Python By building games - Full Course/pong.py/Pong/connect four.py
@@ -1,10 +1,27 @@
 '''
 connect four
 '''
-
+# imports numpy function, set numpy to be defined as np
 import numpy as np
 
-
+# defines the board specifications
 def create_board(
     board = np.zeros((6,7)))
     return board
+
+# creates board
+board = create_board()
+
+# defines if the game is over on in play
+game_over = False
+
+#defines turn amount
+turn = 0
+
+# defines what happens if the game is not over 
+while not game_over:
+    # ask for player 1 input
+    if turn == 0:
+        selection = input("Player 1 make your Selection (0-6):")
+
+    # askfor player 2 input
